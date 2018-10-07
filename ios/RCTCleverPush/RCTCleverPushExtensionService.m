31d24979d628948bde5ba08dc0e21301c2c4223b
marius <marius.gebhardt@gmail.com>
2018-10-07 18:49:37 +0200

    remove extension service

--- ios/RCTCleverPush/RCTCleverPushExtensionService.m
+++ ios/RCTCleverPush/RCTCleverPushExtensionService.m
@@ -9,11 +9,11 @@
 @implementation RCTCleverPushExtensionService
 
 +(void)didReceiveNotificationRequest:(UNNotificationRequest *)request withContent:(UNMutableNotificationContent * _Nullable)content {
-    [CleverPush didReceiveNotificationExtensionRequest:request withMutableNotificationContent:content];
+
 }
 
 +(void)serviceExtensionTimeWillExpireRequest:(UNNotificationRequest *)request withMutableNotificationContent:(UNMutableNotificationContent * _Nullable)content {
-    [CleverPush serviceExtensionTimeWillExpireRequest:request withMutableNotificationContent:content];
+
 }
 
 @end
