04c80e603cffcaa6aea94b0123b5e13beb1d2473
gbhrdt <gbhrdt@users.noreply.github.com>
2018-11-24 17:21:52 +0100

    add notification extension service support

--- ios/RCTCleverPush/RCTCleverPushExtensionService.m
+++ ios/RCTCleverPush/RCTCleverPushExtensionService.m
@@ -8,12 +8,12 @@
 
 @implementation RCTCleverPushExtensionService
 
-+(void)didReceiveNotificationRequest:(UNNotificationRequest *)request withContent:(UNMutableNotificationContent * _Nullable)content {
-
++ (void)didReceiveNotificationRequest:(UNNotificationRequest *)request withContent:(UNMutableNotificationContent * _Nullable)content {
+    [CleverPush didReceiveNotificationExtensionRequest:request withMutableNotificationContent:content];
 }
 
-+(void)serviceExtensionTimeWillExpireRequest:(UNNotificationRequest *)request withMutableNotificationContent:(UNMutableNotificationContent * _Nullable)content {
-
++ (void)serviceExtensionTimeWillExpireRequest:(UNNotificationRequest *)request withMutableNotificationContent:(UNMutableNotificationContent * _Nullable)content {
+    [CleverPush serviceExtensionTimeWillExpireRequest:request withMutableNotificationContent:content];
 }
 
 @end
